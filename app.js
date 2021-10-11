ef7823292ff6f01fa6f0ea657891594c245031ba
nunokf98 <your_email_idnunok98@hotmail.comgit config --global user.name nunokf98git config --global user.email your_email_id>
2021-10-11 16:09:46 +0100

    v3

--- app.js
+++ app.js
@@ -4,21 +4,63 @@ class Competition{ // Criar classe
     name;
     startDate;
     endDate;
+
+
+    constructor(name, startDate,endDate) {
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    toString() {
+        return `${this.name} - (${this.startDate}) (${this.endDate})`;
+    }
 }  
 class Team{
+    competition;
     name;
     points;
+
+    constructor(competition,name) {
+        this.competition = competition;
+        this.name = name;
+    }
+
+    toString() {
+        return `${this.name} - ${this.points}`;
+    }
+    
 }
 
 class Match{
+    competition;
     homeTeam;
     awayTeam;
     location;
     result;
+
+    constructor(competition,homeTeam, awayTeam,location)
+    {
+        this.competition = competition;
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.location = location;
+    }
 }
 
 class Ranking{
+    competition
     team;
     position;
+
+    constructor(competition, team, position) {
+        this.competition = competition;
+        this.team = team
+        this.position = position;;
+    }
+
+    toString() {
+        return `${this.team} - ${this.position}`;
+    }
 }
 
